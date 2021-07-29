33ff87e890af61e35318be1e0cda4633c1b391bf
thomas.kao <thomas.kao@xrspace.io>
2021-07-29 14:39:43 +0800

    doc: comment on process

--- Debug_Win.bat
+++ Debug_Win.bat
@@ -1,6 +1,9 @@
+REM build docker image
 docker build -t myapache2 .
 
 docker rm -f xrspace-apiadaptor-test
+
+REM run to test
 docker run -dit --name xrspace-apiadaptor-test -p 8080:80 myapache2
 
 start "" http://127.0.0.1:8080/index.html
---
+++ colfer schema/build.bat
@@ -0,0 +1,2 @@
+REM https://github.com/pascaldekloe/colfer
+colf -v -p jsadaptor JavaScript nftart.colf response.colf
